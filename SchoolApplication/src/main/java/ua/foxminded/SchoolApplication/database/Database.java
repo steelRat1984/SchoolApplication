a2cea8e8f007e89a10372c4f121168d7e836e965
Vladyslav <pharmsv12@gmail.com>
2024-02-07 14:17:39 +0200

    import Connection

--- SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/database/Database.java
+++ SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/database/Database.java
@@ -1,14 +1,15 @@
 package ua.foxminded.SchoolApplication.database;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Database {
+public class Database {	
 	private static final String CONNECTION = "jdbc:postgresql://localhost:5432/school_app";
 	private static final String USER = "school_admin";
 	private static final String PASSWORD = "1234";
 
-	public static java.sql.Connection connection() {
+	public static Connection connection() {
 		try {
 			return DriverManager.getConnection(CONNECTION, USER, PASSWORD);
 		} catch (SQLException e) {
