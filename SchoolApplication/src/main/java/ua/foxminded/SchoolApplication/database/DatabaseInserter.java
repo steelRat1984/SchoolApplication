b36eb00af22aa1313595193135a488f793f2ba88
Vladyslav <pharmsv12@gmail.com>
2024-02-07 14:33:28 +0200

    remove Connection to try-with-resources

--- SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/database/DatabaseInserter.java
+++ SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/database/DatabaseInserter.java
@@ -11,11 +11,11 @@ import ua.foxminded.SchoolApplication.model.Student;
 import ua.foxminded.SchoolApplication.model.StudentCourseRelation;
 
 public class DatabaseInserter {
-	Connection connection = Database.connection();
 
-	protected void insertCourseData(List<Course> courses) {
+	public void insertCourseData(List<Course> courses) {
 		String sql = "INSERT INTO school_app.courses (course_id, course_name, course_description) VALUES (?, ?, ?)";
-		try (PreparedStatement statement = connection.prepareStatement(sql)) {
+		try (Connection connection = Database.connection();
+				PreparedStatement statement = connection.prepareStatement(sql)) {
 			for (Course course : courses) {
 				statement.setInt(1, course.getCourseID());
 				statement.setString(2, course.getCourseName());
@@ -28,9 +28,10 @@ public class DatabaseInserter {
 		}
 	}
 
-	protected void insertGroupData(List<Group> groups) {
+	public void insertGroupData(List<Group> groups) {
 		String sql = "INSERT INTO school_app.groups (group_id, group_name) VALUES (?, ?)";
-		try (PreparedStatement statement = connection.prepareStatement(sql)) {
+		try (Connection connection = Database.connection();
+				PreparedStatement statement = connection.prepareStatement(sql)) {
 			for (Group group : groups) {
 				statement.setInt(1, group.getGroupID());
 				statement.setString(2, group.getGroupName());
@@ -42,9 +43,10 @@ public class DatabaseInserter {
 		}
 	}
 
-	protected void insertStudentsData(List<Student> students) {
+	public void insertStudentsData(List<Student> students) {
 		String sql = "INSERT INTO school_app.students (student_id, group_id, first_name, last_name) VALUES (?, ?, ?, ?)";
-		try (PreparedStatement statement = connection.prepareStatement(sql)) {
+		try (Connection connection = Database.connection();
+				PreparedStatement statement = connection.prepareStatement(sql)) {
 			for (Student student : students) {
 				statement.setInt(1, student.getStudentID());
 				statement.setInt(2, student.getGroupID());
@@ -56,12 +58,12 @@ public class DatabaseInserter {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
 	}
 
-	protected void insertRelations(List<StudentCourseRelation> relations) {
+	public void insertRelations(List<StudentCourseRelation> relations) {
 		String sql = "INSERT INTO school_app.students_courses_relations (student_id, course_id) VALUES (?, ?)";
-		try (PreparedStatement statement = connection.prepareStatement(sql)) {
+		try (Connection connection = Database.connection();
+				PreparedStatement statement = connection.prepareStatement(sql)) {
 			for (StudentCourseRelation relation : relations) {
 				statement.setInt(1, relation.getStudentID());
 				statement.setInt(2, relation.getCourseID());
