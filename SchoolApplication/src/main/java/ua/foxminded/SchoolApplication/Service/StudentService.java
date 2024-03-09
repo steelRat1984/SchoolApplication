03e1f535c70dbada3a14348f7e52124fd7e90cda
Vladyslav <pharmsv12@gmail.com>
2024-03-09 21:57:54 +0200

    delete getSelectedCoursesID method

--- SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/Service/StudentService.java
+++ SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/Service/StudentService.java
@@ -1,10 +1,7 @@
 package ua.foxminded.SchoolApplication.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 import ua.foxminded.SchoolApplication.database.CourseDAO;
