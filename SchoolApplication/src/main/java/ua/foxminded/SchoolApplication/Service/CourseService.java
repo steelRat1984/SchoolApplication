d755413efaefc8000055d7d838b8c8dc17ac113c
Vladyslav <pharmsv12@gmail.com>
2024-03-10 22:14:05 +0200

    replace countStudentsByCourse method

--- SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/Service/CourseService.java
+++ SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/Service/CourseService.java
@@ -3,27 +3,18 @@ package ua.foxminded.SchoolApplication.Service;
 import java.util.List;
 
 import ua.foxminded.SchoolApplication.database.CourseDAO;
-import ua.foxminded.SchoolApplication.database.StudentDAO;
 import ua.foxminded.SchoolApplication.model.Course;
-import ua.foxminded.SchoolApplication.model.Student;
 
 public class CourseService {
-	CourseDAO courseDAO = new CourseDAO();
-	StudentDAO studentDAO = new StudentDAO();
-	
-	public Integer countStudentsByCourse (int courseId) {
-		List<Student> students = studentDAO.getEnrolledStudents(courseId);
-		int result = students.size();
-		return result;
-	}
+	private CourseDAO courseDAO = new CourseDAO();
 	
 	public Course getCourseById(int courseId) {
 		Course course = courseDAO.getCourseById(courseId);
 		return course;	
 	}
 	
-	public List<Course> getCourses(){
-		List<Course> coursesList = courseDAO.selectAllCourses();
+	public List<Course> getAllCourses(){
+		List<Course> coursesList = courseDAO.getAllCourses();
 		return coursesList;
 	}
 }
