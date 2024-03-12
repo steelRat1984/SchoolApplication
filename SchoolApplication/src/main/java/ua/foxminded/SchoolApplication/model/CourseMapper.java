b535087307ee12df3e098dd0aa3bc08b8d2ae038
Vladyslav <pharmsv12@gmail.com>
2024-03-12 20:31:11 +0200

    mistake correction

--- SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/model/CourseMapper.java
+++ SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/model/CourseMapper.java
@@ -8,8 +8,8 @@ import ua.foxminded.SchoolApplication.database.CourseDAO;
 public class CourseMapper {
 	public static Course map(ResultSet resultSet) throws SQLException {
 		int courseId = resultSet.getInt("course_id");
-		String courseName = resultSet.getString("first_name");
-		String courseDesctiption = resultSet.getString("last_name");
+		String courseName = resultSet.getString("course_name");
+		String courseDesctiption = resultSet.getString("course_description");
 		int numberOfStudents = new CourseDAO().countStudentOnCourse(courseId);
 		return new Course(courseId, numberOfStudents, courseName, courseDesctiption);
 	}
