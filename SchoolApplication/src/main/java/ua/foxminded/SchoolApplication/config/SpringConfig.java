ea87a86bee7a20fd0658cd1f5bdcafa19cd2cef7
Vladyslav <pharmsv12@gmail.com>
2024-05-09 16:41:25 +0300

    add database.properties

--- SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/config/SpringConfig.java
+++ SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/config/SpringConfig.java
@@ -8,28 +8,34 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @ComponentScan("ua.foxminded.SchoolApplication")
+@PropertySource("classpath:database.properties")
 public class SpringConfig {
 	
+	private final Environment environment;
 	private final ApplicationContext applicationContext;
-
+	
 	@Autowired
-	public SpringConfig(ApplicationContext applicationContext) {
+	public SpringConfig(Environment environment, ApplicationContext applicationContext) {
+		this.environment = environment;
 		this.applicationContext = applicationContext;
 	}
+
 
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/school_app");
-		dataSource.setUsername("school_admin");
-		dataSource.setPassword("1234");
+		dataSource.setDriverClassName(environment.getProperty("driver"));
+		dataSource.setUrl(environment.getProperty("url"));
+		dataSource.setUsername(environment.getProperty("username"));
+		dataSource.setPassword(environment.getProperty("password"));
 
 		return dataSource;
 	}
