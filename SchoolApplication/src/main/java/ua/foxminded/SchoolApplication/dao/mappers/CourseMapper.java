c6d635e37edf44c1405aba3e41b027c1428fac63
Vladyslav <pharmsv12@gmail.com>
2024-05-09 15:43:03 +0300

    trying to create new mappers

--- SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/dao/mappers/CourseMapper.java
+++ SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/dao/mappers/CourseMapper.java
@@ -3,13 +3,26 @@ package ua.foxminded.SchoolApplication.dao.mappers;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.springframework.jdbc.core.RowMapper;
+
 import ua.foxminded.SchoolApplication.model.Course;
 
-public class CourseMapper {
+public class CourseMapper implements RowMapper<Course>{
+	
+	
+	
 	public static Course map(ResultSet resultSet) throws SQLException {
 		int courseId = resultSet.getInt("course_id");
 		String courseName = resultSet.getString("course_name").trim();
 		String courseDesctiption = resultSet.getString("course_description").trim();
+		return new Course(courseId, courseName, courseDesctiption);
+	}
+
+	@Override
+	public Course mapRow(ResultSet rs, int rowNum) throws SQLException {
+		int courseId = rs.getInt("course_id");
+		String courseName = rs.getString("course_name").trim();
+		String courseDesctiption = rs.getString("course_description").trim();
 		return new Course(courseId, courseName, courseDesctiption);
 	}
 }
--- SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/dao/mappers/GroupMapper.java
+++ SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/dao/mappers/GroupMapper.java
@@ -3,13 +3,22 @@ package ua.foxminded.SchoolApplication.dao.mappers;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.springframework.jdbc.core.RowMapper;
+
 import ua.foxminded.SchoolApplication.model.Group;
 
-public class GroupMapper {
+public class GroupMapper implements RowMapper<Group> {
 
 	public static Group map(ResultSet resultSet) throws SQLException {
 		int groupId = resultSet.getInt("group_id");
 		String groupName = resultSet.getString("group_name").trim();
+		return new Group(groupId, groupName);
+	}
+
+	@Override
+	public Group mapRow(ResultSet rs, int rowNum) throws SQLException {
+		int groupId = rs.getInt("group_id");
+		String groupName = rs.getString("group_name").trim();	
 		return new Group(groupId, groupName);
 	}
 }
