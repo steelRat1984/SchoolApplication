346b97084d362034f0bd1c8cc2d72b2288740f8c
Vladyslav <pharmsv12@gmail.com>
2024-05-23 13:35:49 +0300

    delete treeMap

--- SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/dao/mappers/StudentResultSetExtractor.java
+++ SchoolApplication/src/main/java/ua/foxminded/SchoolApplication/dao/mappers/StudentResultSetExtractor.java
@@ -3,6 +3,7 @@ package ua.foxminded.SchoolApplication.dao.mappers;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -28,15 +29,15 @@ public class StudentResultSetExtractor implements ResultSetExtractor<List<Studen
 
     @Override
     public List<Student> extractData(ResultSet rs) throws SQLException, DataAccessException {
-        Map<Integer, Student> idAndStudents = new TreeMap<>();
+        Map<Integer, Student> students = new HashMap<Integer, Student>();
         while (rs.next()) {
             int studentId = rs.getInt("student_id");
-            Student student = idAndStudents.get(studentId);
+            Student student = students.get(studentId);
 
             if (student == null) {
                 student = studentMapper.mapRow(rs, rs.getRow());
                 student.setCourses(new ArrayList<>());
-                idAndStudents.put(studentId, student);
+                students.put(studentId, student);
             }
 
             int courseId = rs.getInt("course_id");
@@ -45,7 +46,7 @@ public class StudentResultSetExtractor implements ResultSetExtractor<List<Studen
                 student.getCourses().add(course);
             }
         }
-        return new ArrayList<>(idAndStudents.values());
+        return new ArrayList<>(students.values());
     }
 
 }
