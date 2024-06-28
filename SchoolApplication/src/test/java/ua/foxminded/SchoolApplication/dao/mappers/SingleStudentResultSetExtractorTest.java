a9c9935f43b08665168c87d0114816d25b82d8a5
Vladyslav <pharmsv12@gmail.com>
2024-06-29 00:14:04 +0300

    extractor classes test covering

--- SchoolApplication/src/test/java/ua/foxminded/SchoolApplication/dao/mappers/SingleStudentResultSetExtractorTest.java
+++ SchoolApplication/src/test/java/ua/foxminded/SchoolApplication/dao/mappers/SingleStudentResultSetExtractorTest.java
@@ -31,35 +31,27 @@ public class SingleStudentResultSetExtractorTest {
 
 	@InjectMocks
 	private SingleStudentResultSetExtractor extractor;
+	
+	private Student student;
+    private Course course1;
+    private Course course2;
 
-	@BeforeEach
-	public void setUp() throws SQLException {
-		MockitoAnnotations.openMocks(this);
+	  @BeforeEach
+	    public void setUp() throws SQLException {
+	        MockitoAnnotations.openMocks(this);
+	        
+	        Group group = new Group(1, "Group A");
+	        student = new Student(1, group, "John", "Doe", new ArrayList<>());
+	        course1 = new Course(1, "Math", "Math course");
+	        course2 = new Course(2, "Science", "Science course");
 
-		when(rs.next()).thenReturn(true, true, false);
-		when(rs.getInt("student_id")).thenReturn(1);
-		when(rs.getString("first_name")).thenReturn("John");
-		when(rs.getString("last_name")).thenReturn("Doe");
-		when(rs.getInt("group_id")).thenReturn(1);
-		when(rs.getString("group_name")).thenReturn("Group A");
-		when(rs.getInt("course_id")).thenReturn(1, 2);
-		when(rs.getString("course_name")).thenReturn("Math", "Science");
-		when(rs.getString("course_description")).thenReturn("Math course", "Science course");
-		when(studentMapper.mapRow(any(ResultSet.class), anyInt())).thenAnswer(invocation -> {
-			ResultSet rsMock = invocation.getArgument(0);
-			Group group = new Group(rsMock.getInt("group_id"), rsMock.getString("group_name").trim());
-			return new Student(rsMock.getInt("student_id"), group, rsMock.getString("first_name").trim(),
-					rsMock.getString("last_name").trim(), new ArrayList<>());
-		});
-		when(courseMapper.mapRow(any(ResultSet.class), anyInt())).thenAnswer(invocation -> {
-			ResultSet rsMock = invocation.getArgument(0);
-			int courseId = rsMock.getInt("course_id");
-			String courseName = rsMock.getString("course_name").trim();
-			String courseDescription = rsMock.getString("course_description").trim();
-			return new Course(courseId, courseName, courseDescription);
-		});
+	        when(studentMapper.mapRow(any(ResultSet.class), anyInt())).thenReturn(student);
+	        when(courseMapper.mapRow(any(ResultSet.class), anyInt())).thenReturn(course1).thenReturn(course2);
 
-	}
+	        when(rs.next()).thenReturn(true, true, false); 
+	        when(rs.getInt("course_id")).thenReturn(1).thenReturn(2);
+	        when(rs.wasNull()).thenReturn(false);
+	    }
 
 	@Test
 	public void shoudNotReturnNullStudentAndCourses() throws DataAccessException, SQLException {
@@ -70,7 +62,7 @@ public class SingleStudentResultSetExtractorTest {
 	}
 
 	@Test
-	public void shouldReturnProperStudentFields() throws DataAccessException, SQLException {
+	public void shouldReturnCorrectStudentFields() throws DataAccessException, SQLException {
 		Student actualStudent = extractor.extractData(rs);
 		assertEquals(1, actualStudent.getStudentID());
 		assertEquals("John", actualStudent.getFirstName());
@@ -79,7 +71,7 @@ public class SingleStudentResultSetExtractorTest {
 	}
 
 	@Test
-	public void shoudReturnProperCourseFields() throws DataAccessException, SQLException {
+	public void shoudReturnCorrectCourseFields() throws DataAccessException, SQLException {
 		Student actualStudent = extractor.extractData(rs);
 		Course actualCourse1 = actualStudent.getCourses().stream()
 				.filter(course -> course.getCourseName() == "Math")
